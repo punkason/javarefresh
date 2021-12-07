9e7a8e427d9756df7294f98d99630427b1ba180a
punkason <punkason@gmail.com>
2021-12-07 15:55:02 +0000

    Completed ex2

--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -4,11 +4,17 @@ public class Exercise002 {
 
     public boolean isFromManchester(Person person) {
         // Add your code here
-        return false;
+    	String city = person.getCity();
+    	if (city.equals("Manchester"))
+        	return true;
+    	return false;
     }
 
-    public boolean canWatchFilm(/* Person person, int ageLimit*/) {
+    public boolean canWatchFilm(Person person, int ageLimit) {
         // Add your code here
+    	int age = person.getAge();
+    	if ( age >= ageLimit)
+    		return true;
         return false;
     }
     
